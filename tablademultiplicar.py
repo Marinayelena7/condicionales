54666c8bb253b964c8a88699277e749610d35c34
Marinayelena7 <lopez.negrete.elena@iescamas.es>
2024-10-22 10:51:06 +0200

    Add files via upload

---
+++ tablademultiplicar.py
@@ -0,0 +1,5 @@
+#esto es un programa para las tablas de multiplicar.
+print("DIME UN NUMERO DE LA TABLA DE MULTIPLICAR")
+num_tabla=int(input())
+for i in range (11):
+    print(num_tabla,"x", i, "=", num_tabla*i)
